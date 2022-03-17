dd40fc052bf15e3c7701c33bb3a2ab4d55a333dc
sohumsikdar <sohum20339@iiitd.ac.in>
2022-03-18 04:15:52 +0530

    erosion and dilution

--- basic_functions.py
+++ basic_functions.py
@@ -1,28 +1,57 @@
 import cv2
+import numpy as np
+
+
+def read_img():
+	img = cv2.imread('Resources/prague.png')
+	return img
+
+
+def show_img(img):
+	cv2.imshow('img', img)
+	cv2.waitKey(0)
 
 
 def grey_img():
-	img = cv2.imread('Resources/prague.png')
+	img = read_img()
 	img_greyscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	cv2.imshow('grey scale img', img_greyscale)
-	cv2.waitKey(0)
+	# show_img(img_greyscale)
+	return img_greyscale
 
 
 def blur_img():
-	img = cv2.imread('Resources/prague.png')
+	img = read_img()
 	img_blurred = cv2.GaussianBlur(img, (5, 5), 1)
-	cv2.imshow('blurred img', img_blurred)
-	cv2.waitKey(0)
+	# show_img(img_blurred)
+	return img_blurred
 
 
 def canny_img():
-	img = cv2.imread('Resources/prague.png')
+	img = read_img()
 	img_canny = cv2.Canny(img, 100, 100)
-	cv2.imshow('Canny img', img_canny)
-	cv2.waitKey(0)
+	# show_img(img_canny)
+	return img_canny
+
+
+def dilated_img():
+	kernel = np.ones((5,5), np.uint8)
+	img = canny_img()
+	img_dilation = cv2.dilate(img, kernel, iterations=1)
+	show_img(img_dilation)
+	return img_dilation
+
+
+def eroded_img():
+	kernel = np.ones((5, 5), np.uint8)
+	img = dilated_img()
+	img_erosion = cv2.erode(img, kernel, iterations=1)
+	show_img(img_erosion)
+	return img_erosion
 
 
 if __name__ == '__main__':
-	grey_img()
-	blur_img()
-	canny_img()
+	# grey_img()
+	# blur_img()
+	# canny_img()
+	eroded_img()
+	# dilated_img()
